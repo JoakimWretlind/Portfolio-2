a84c1d6d6998dde14cf8ed5da7845fc3c1fc0806
Joakim Wretlind <joakim.wretlind@gmail.com>
2021-09-23 18:49:41 +0200

    testing styling with mail-function

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,30 +1,44 @@
 import React from 'react'
 import { Section, InnerContainer } from '../../components/sharedStyling'
 import Header from '../../components/Header'
+import {
+    ContentWrapper,
+    LeftSection,
+    RightSection
+} from './contact.style';
 
 const Contact = () => {
     return (
         <Section id="contact" style={{ background: "#666" }}>
             <InnerContainer>
                 <Header title="contact" />
+
+
                 <form name="contact" method="post">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p>
-                        <label htmlFor="name">Name</label> <br />
-                        <input type="text" id="name" name="name" required />
-                    </p>
-                    <p>
-                        <label htmlFor="email">Email</label> <br />
-                        <input type="email" id="email" name="email" required />
-                    </p>
-                    <p>
-                        <label htmlFor="message">Message</label> <br />
-                        <textarea id="message" name="message" required></textarea>
-                    </p>
-                    <p>
-                        <input type="submit" value="Submit message" />
-                    </p>
+                    <ContentWrapper>
+                        <LeftSection>
+                            <input type="hidden" name="form-name" value="contact" />
+                            <p>
+                                <label htmlFor="name">Name</label> <br />
+                                <input type="text" id="name" name="name" required />
+                            </p>
+                            <p>
+                                <label htmlFor="email">Email</label> <br />
+                                <input type="email" id="email" name="email" required />
+                            </p>
+                        </LeftSection>
+                        <RightSection>
+                            <p>
+                                <label htmlFor="message">Message</label> <br />
+                                <textarea id="message" name="message" required></textarea>
+                            </p>
+                            <p>
+                                <input type="submit" value="Submit message" />
+                            </p>
+                        </RightSection>
+                    </ContentWrapper>
                 </form>
+
             </InnerContainer>
         </Section>
     )
---
+++ src/pages/Contact/contact.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ContentWrapper = styled.div`
+    border: 2px solid yellow;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+export const LeftSection = styled.div`
+    border: 2px solid red;
+    width: 50%;
+    min-width: 28.2rem;
+`;
+
+export const RightSection = styled.div`
+    border: 2px solid blue;
+    width: 50%;
+    min-width: 28.2rem;
+    
+`;
